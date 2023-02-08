ac43e62848b0000d43de600ff49bae464f38c708
flestnia <cendradewangga@gmail.com>
2023-02-08 17:06:01 +0700

    try to deploy 4

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,7 +4,7 @@ import TmpMain from "./tmp-main";
 //pages
 import PgAuth from "./pg-auth";
 import PgHome from "./pg-home";
-// import PgProfile from "./pg-profile";
+import PgProfile from "./pg-profile";
 import PgHistory from "./pg-history";
 
 export { TmpMain, PgAuth, PgHome, PgHistory };
